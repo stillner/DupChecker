b21d61283229275069434229f887a2c9a16d5148
Stefan Illner <stillner@moll-illner.org>
2014-04-21 16:44:43 +0200

    First tree display in GUI - ugly, but working.
    Currently only for a fixed directory.

--- org/moll_illner/dupChecker/DupChecker.java
+++ org/moll_illner/dupChecker/DupChecker.java
@@ -14,9 +14,9 @@ public class DupChecker {
         List<StartingPoint> startingPoints = new ArrayList<StartingPoint>();
         startingPoints.add(new StartingPoint("/home/stillner/dupdir"));
         Crawler crawler = new Crawler(startingPoints);
-        Map<Long, List<FileRef>> duplicates = crawler.crawl();
-        if (duplicates != null) {
-            for (Map.Entry<Long, List<FileRef>> entry: duplicates.entrySet()) {
+        Map<Long, List<FileRef>> duplicateFilesBySize = crawler.crawl();
+        if (duplicateFilesBySize != null) {
+            for (Map.Entry<Long, List<FileRef>> entry: duplicateFilesBySize.entrySet()) {
                 System.out.println();
                 System.out.println(entry.getKey());
                 List<FileRef> files = entry.getValue();
@@ -29,7 +29,8 @@ public class DupChecker {
         }
         
         MainWindow w = new MainWindow();
-        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        w.displayFiles(duplicateFilesBySize);
         w.setVisible(true);
     }
 
--- org/moll_illner/dupChecker/ui/MainWindow.java
+++ org/moll_illner/dupChecker/ui/MainWindow.java
@@ -2,14 +2,23 @@ package org.moll_illner.dupChecker.ui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import org.moll_illner.dupChecker.FileRef;
 
 public class MainWindow extends JFrame implements ActionListener {
     
     private JMenuItem _quitMenuItem;
+    private DefaultMutableTreeNode _rootNode;
     
     public MainWindow() {
         
@@ -18,22 +27,45 @@ public class MainWindow extends JFrame implements ActionListener {
         JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
         
-        JMenuItem fileMenuItem = new JMenuItem("File");
-        menuBar.add(fileMenuItem);
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+        
+        JPanel mainPanel = new JPanel();
+        this.add(mainPanel);
+        
+        _rootNode = new DefaultMutableTreeNode("Root node");
+        JTree tree = new JTree(_rootNode);
+        mainPanel.add(tree);
         
         _quitMenuItem = new JMenuItem("Quit");
-        fileMenuItem.add(_quitMenuItem);
+        fileMenu.add(_quitMenuItem);
         
         _quitMenuItem.addActionListener(this);
     }
 
+    
     public void actionPerformed(ActionEvent evt) {
         if (evt.getSource() == _quitMenuItem) {
             System.exit(0);
         }
-        
     }
+
     
-    
+    public void displayFiles(Map<Long, List<FileRef>> filesBySize) {
+        if (filesBySize != null) {
+            for (Map.Entry<Long, List<FileRef>> entry: filesBySize.entrySet()) {
+                DefaultMutableTreeNode sizeNode = new DefaultMutableTreeNode(entry.getKey().toString());
+                List<FileRef> files = entry.getValue();
+                if (files != null) {
+                    for (FileRef f: files) {
+                        DefaultMutableTreeNode fileNode = new DefaultMutableTreeNode(f.getPath());
+                        sizeNode.add(fileNode);
+                    }
+                }
+                _rootNode.add(sizeNode);
+            }
+        }
+
+    }
 
 }
